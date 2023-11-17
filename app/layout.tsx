0a37c6d291c3b9f03ad3656fe0ef2809524c4c16
R3Kr <thefungigg@gmail.com>
2023-11-17 22:02:24 +0100

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbars from "@/components/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className + " bg-slate-800"}>
         <Navbars></Navbars>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
