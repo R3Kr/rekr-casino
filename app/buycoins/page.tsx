8da81a07a7a44a1a2c7de1f01cbb1e108ddd8ef2
R3Kr <thefungigg@gmail.com>
2023-11-19 20:12:32 +0100

    qwe

--- app/buycoins/page.tsx
+++ app/buycoins/page.tsx
@@ -1,16 +1,13 @@
-"use client"
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default function page() {
-    const router = useRouter();
- 
-    useEffect(() => {
-        router.push("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    }, [])
+export default function Page() {
+  const router = useRouter();
 
+  useEffect(() => {
+    router.push("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  });
 
-  return (
-    <div>:)</div>
-  )
+  return <div>:)</div>;
 }
