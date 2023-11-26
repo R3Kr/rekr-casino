a901fada1d497eb190f6d5e209ab222a008b4230
R3Kr <thefungigg@gmail.com>
2023-11-26 22:11:17 +0100

    qwe

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -50,7 +50,7 @@ export default async function Page() {
           </tr>
         ))}
       </table>
-      <Test n={Math.random()}></Test>
+      <Test n={Math.random() + 0.1}></Test>
     </Marquee>
   );
 }
