f2a9b1755fe771b9ff31354e4c1d953c15a75584
R3Kr <thefungigg@gmail.com>
2024-03-05 15:27:18 +0100

    if poke then unpoke

--- app/poke/PokePerson.tsx
+++ app/poke/PokePerson.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { User } from "@prisma/client";
 import Image from "next/image";
-import { poke } from "@/lib/actions";
+import { poke, unpoke } from "@/lib/actions";
 
 type Props = Pick<User, "id" | "name" | "image">;
 
@@ -10,7 +10,13 @@ export default function PokePerson({ id, name, image }: Props) {
   return (
     <div className="flex flex-row p-1 gap-2">
       {image ? <Image src={image} alt="pp" width={100} height={100} /> : name}
-      <button className="bg-green-500" onClick={() => poke(id)}>
+      <button
+        className="bg-green-500"
+        onClick={() => {
+          poke(id);
+          unpoke();
+        }}
+      >
         Poke
       </button>
     </div>
