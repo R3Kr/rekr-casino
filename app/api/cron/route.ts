a6fddb7f1e7ef2cadb26258ff88ae3ec5480dc24
R3Kr <thefungigg@gmail.com>
2024-03-05 20:22:48 +0100

    asd

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,10 +1,13 @@
-import prisma from '@/lib/db';
-import { NextRequest, NextResponse } from 'next/server';
+import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    // if (req.headers.get('authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-    //     return NextResponse.json({}, {status: 401, statusText: "Unauthorized"}) 
-    //   }
-    const enfuckingexploiter = await prisma.user.update({where: {id: "clp5t5ksa0000cp05iqqhrdzg"}, data: {rekr_coins: {decrement: 10000000000}}})
-  return NextResponse.json(enfuckingexploiter);
+  // if (req.headers.get('authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+  //     return NextResponse.json({}, {status: 401, statusText: "Unauthorized"})
+  //   }
+  const enfuckingexploiter = await prisma.user.update({
+    where: { id: "clp5t5ksa0000cp05iqqhrdzg" },
+    data: { rekr_coins: { decrement: BigInt("10000000000") } },
+  });
+  return NextResponse.json({oK: true});
 }
